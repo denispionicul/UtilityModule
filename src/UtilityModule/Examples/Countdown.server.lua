5f5732102ddb6fc896635e51ead3ee4fb3170592
denispionicul <bogdenispionicul@gmail.com>
2023-06-18 16:36:30 +0200

    Fixed fixes and fixes

---
+++ src/UtilityModule/Examples/Countdown.server.lua
@@ -0,0 +1,22 @@
+local UtilityModule = require(script.Parent.Parent)
+local Countdown = UtilityModule.newCountdown(30)
+local RandomEvent = Instance.new("BindableEvent")
+
+Countdown.UpdateEvent = RandomEvent.Event
+
+Countdown:Start(30)
+
+Countdown.OnCount:Connect(function(timer)
+    print(timer)
+end)
+
+local Task = task.spawn(function()
+    while true do
+        task.wait(5)
+        RandomEvent:Fire()
+    end
+end)
+
+Countdown.OnFinished:Connect(function()
+   task.cancel(Task)
+end)
